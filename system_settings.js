0f0a6771ccd53dccb11701aec98669ca1ca64612
Edward Taylor <edeetee@gmail.com>
2019-09-16 16:38:02 +1200

    reset smoothing

--- system_settings.js
+++ system_settings.js
@@ -1,4 +1,4 @@
 // smoothing is applied to the audio file to smooth the signal
 // expected values are from 0 (no smoothing) to 100 (lots of smoothing)
-const smoothing = 70;
+const smoothing = 25;
 const debugFastRefresh = true;
